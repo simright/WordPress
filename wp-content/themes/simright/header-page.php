eaf98b974bdcf3c3d9ca6349d833020a8e63d7d4
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-04-13 15:32:47 +0800

    fix(style):partner

--- wp-content/themes/simright/header-page.php
+++ wp-content/themes/simright/header-page.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v5.0" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v7.0" type="text/css" media="screen" />
 </head>
