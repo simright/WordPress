8e2e4acaeff5928a704bd90d987735684ffc3442
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-04-24 12:14:55 +0800

    fix(furniture):家具专题

--- wp-content/themes/simright/footer-structural.php
+++ wp-content/themes/simright/footer-structural.php
@@ -1,47 +1,78 @@
 <footer>
-        <section class="bg-blue section-wrap">
-            <div class="footer">
-                <div class="footer-contact">
-                    <div class="footer-sales">
-                        <h4><?php pll_e('Email:'); ?></h4>
-                        <p><a href="mailto:info@simright.com">info@simright.com</a></p>
-                    </div>
-                </div>
-                <div class="footer-products">
-                    <h4><?php pll_e('Products'); ?></h4>
-                    <p><a href="/apps/simright-simulator"><i class="fa fa-angle-right"></i><b>Simulator</b></a></li>
-                    <p><a href="/apps/simright-toptimizer"><i class="fa fa-angle-right"></i><b>Toptimizer</b></a></li>
-                    <p><a href="/apps/simright-webmesher"><i class="fa fa-angle-right"></i><b>WebMesher</b></a></li>
-                    <p><a href="/apps/simright-viewer"><i class="fa fa-angle-right"></i><b>Viewer</b></a></li>
-                    <p><a href="/apps/simright-converter"><i class="fa fa-angle-right"></i><b>CAE Converter</b></a></li>
-                    <p><a href="/apps/simright-cad-converter"><i class="fa fa-angle-right"></i><b>CAD Converter</b></a></li>
-                </div>
-                <div class="footer-follow">
-                    <h4>Follow Us</h4>
-                    <div>
-                        <a href="https://www.facebook.com/Simright-1913454768934744" target="_blank"><img src="https://oss.simright.com/images/contact-facebook.svg" alt="Facebook"></a>
-                        <a href="https://twitter.com/simright" target="_blank"><img src="https://oss.simright.com/images/contact-twiiter.svg" alt="Twitter"></a>
-                        <a href="//shang.qq.com/wpa/qunwpa?idkey=15506675f16f72139d7c9632c62ac0f567fde75cdad3570137f31ac7bfce7624" target="_blank"><img src="https://oss.simright.com/images/contact-qq.svg" alt="QQ"></a>
-                        <a href="http://weibo.com/u/6210090560" target="_blank"><img src="https://oss.simright.com/images/contact-weibo.svg" alt="Weibo"></a>
-                        <a href=" https://www.linkedin.com/company-beta/13345726/" target="_blank"><img src="https://oss.simright.com/images/contact-insgram.svg" alt="In"></a>
-                        <a href="http://www.toutiao.com/c/user/59020482121/" target="_blank"><img src="https://oss.simright.com/images/contact-toutiao.svg" alt="Toutiao"></a>
-                        <a href="http://i.youku.com/simright" target="_blank"><img src="https://oss.simright.com/images/contact-youku.svg" alt="Youku"></a>
-                        <a href="https://www.youtube.com/channel/UCEiDfhLf7jFWJkPhhdwpW3g" target="_blank"><img src="https://oss.simright.com/images/contact-youtube.svg" alt="Youtube"></a>
-                    </div>
-                    <?php $current_lan = pll_current_language(); if($current_lan == 'en'): ?>
-                        <img src="https://oss.simright.com/images/rq_code.png" alt="" class="wechat_qr_code hide ">
-                    <?php else : ?>
-                        <img src="https://oss.simright.com/images/rq_code.png" alt="" class="wechat_qr_code ">
-                    <?php endif; ?>
-                </div>
+    <section class="bg-blue section-wrap">
+        <div class="footer">
+            <div class="footer-products">
+                <h4>
+                    <?php pll_e('Public Cloud Apps'); ?>
+                </h4>
+                <ul>
+                    <li><a href="/apps/simright-simulator"><?php pll_e('Simulator - Structural Analysis'); ?></a></li>
+                    <li><a href="/apps/simright-toptimizer"><?php pll_e('Toptimizer - Topology Optimization'); ?></a></li>
+                    <li><a href="/apps/simright-webmesher"><b><?php pll_e('WebMesher – Pre-processor'); ?></b></a></li>
+                    <li><a href="/apps/simright-viewer"><?php pll_e('Viewer - CAD/CAE model viewer'); ?></a></li>
+                    <li><a href="/apps/simright-converter"><?php pll_e('CAE Converter - CAE model converter'); ?></a></li>
+                    <li><a href="/apps/simright-cad-converter"><?php pll_e('CAD Converter - CAD model converter'); ?></a></li>
+                    <li><a href="/apps/simright-threemagic"><?php pll_e('ThreeMagic - STL editor for 3D printing'); ?></a></li>
+                    <?php if(pll_current_language()=="zh"): ?>
+                    <li class="sub-app"><a href="http://www.easypdm.cn/">EasyPDM - 在线三维数据协同</a></li>
+                    <?php endif ?>
+                </ul>
             </div>
-        </section>
-        <section class="clearfix copyright">
-            <p class="text-center">
-                Copyright © 2018 Simright Information Technology Co.,Ltd. All Rights Reserved.
-            <p class="text-center">Powered By <a rel="external" title="WordPress" class="link" href="http://wordpress.org/">WordPress</a></p>
-        </section>
-    </footer>
+            <div class="footer-resources">
+                <h4><?php pll_e('Resources'); ?></h4>
+                <ul>
+                    <li><a href="/resources/public-projects"><?php pll_e('Public Projects'); ?></a></li>
+                    <li><a href="/resources/model-library"><?php pll_e('Model Library'); ?></a></li>
+                    <li><a href="/<?php echo pll_current_language() ?>/category/partners"><?php pll_e('Partners'); ?></a></li>
+                </ul>
+            </div>
+            <div class="footer-learning">
+                <h4>
+                    <?php pll_e('Learning'); ?>
+                </h4>
+                <ul>
+                    <li><a href="/<?php echo pll_current_language() ?>/category/video"><?php pll_e('Video Library'); ?></a></li>
+                    <li><a href="/changelog" ><?php pll_e('Changelog'); ?></a></li>
+                    <li><a href="/<?php echo pll_current_language() ?>/category/features" ><?php pll_e('Features'); ?></a></li>
+                </ul>
+            </div>
+            <div class="footer-about">
+                <h4> 
+                    <?php pll_e('About'); ?>
+                </h4>
+                <ul>
+                    <li><a href="/contact_us"><?php pll_e('Contact Us'); ?></a></li>
+                    <li><a href="/<?php echo pll_current_language() ?>/category/joinus"><?php pll_e('Join Us'); ?></a></li>
+                    <li><a href="/<?php echo pll_current_language() ?>/category/news"><?php pll_e('News'); ?></a></li>
+                    <li><a href="/security"><?php pll_e('Security'); ?></a></li>
+                </ul>
+            </div>
+            <div class="footer-follow">
+                <h4><?php pll_e('Follow Us'); ?></h4>
+                <div>
+                    <a href="https://www.facebook.com/Simright-1913454768934744" target="_blank"><img src="https://oss.simright.com/images/contact-facebook.svg" alt="Facebook"></a>
+                    <a href="https://twitter.com/simright" target="_blank"><img src="https://oss.simright.com/images/contact-twiiter.svg" alt="Twitter"></a>
+                    <a href="//shang.qq.com/wpa/qunwpa?idkey=15506675f16f72139d7c9632c62ac0f567fde75cdad3570137f31ac7bfce7624" target="_blank"><img src="https://oss.simright.com/images/contact-qq.svg" alt="QQ"></a>
+                    <a href="http://weibo.com/u/6210090560" target="_blank"><img src="https://oss.simright.com/images/contact-weibo.svg" alt="Weibo"></a>
+                    <a href=" https://www.linkedin.com/company-beta/13345726/" target="_blank"><img src="https://oss.simright.com/images/contact-insgram.svg" alt="In"></a>
+                    <a href="http://www.toutiao.com/c/user/59020482121/" target="_blank"><img src="https://oss.simright.com/images/contact-toutiao.svg" alt="Toutiao"></a>
+                    <a href="http://i.youku.com/simright" target="_blank"><img src="https://oss.simright.com/images/contact-youku.svg" alt="Youku"></a>
+                    <a href="https://www.youtube.com/channel/UCEiDfhLf7jFWJkPhhdwpW3g" target="_blank"><img src="https://oss.simright.com/images/contact-youtube.svg" alt="Youtube"></a>
+                </div>
+                <?php $current_lan = pll_current_language(); if($current_lan == 'en'): ?>
+                    <img src="https://oss.simright.com/images/rq_code.png" alt="" class="wechat_qr_code hide ">
+                <?php else : ?>
+                    <img src="https://oss.simright.com/images/rq_code.png" alt="" class="wechat_qr_code ">
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+    <section class="clearfix copyright">
+        <p class="text-center">
+            Copyright © 2018 Simright Information Technology Co.,Ltd. All Rights Reserved.
+        <p class="text-center">Powered By <a rel="external" title="WordPress" class="link" href="http://wordpress.org/">WordPress</a></p>
+    </section>
+</footer>
 </body>
 <script>
     document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
@@ -49,7 +80,6 @@
 <script src="https://oss.simright.com/static/jquery.min.js"></script>
 <script src="https://oss.simright.com/static/siema.min.js"></script>
 <script>
-    
     initSwiper()
     function initSwiper(){
         
---
+++ wp-content/themes/simright/page-furniture.php
@@ -0,0 +1,71 @@
+<?php   
+/*   
+Template Name: furniture  
+*/   
+?>
+<?php get_header(); ?>
+<section class="main-box page-furniture">
+    <section class="banner">
+        <img src="https://oss.simright.com/images/cms_furniture_bg.jpg" alt="front">
+    </section>
+    <section class="table furniture-item">
+        <div class="title">
+            <img src="https://oss.simright.com/images/cms_furniture_title-left.png" alt="">
+            <span>桌子</span>
+            <img src="https://oss.simright.com/images/cms_furniture_title-right.png" alt="">
+        </div>
+        <div class="content">
+            <div class="left">
+                <div class="cover-img">
+                    <img src="https://oss.simright.com/images/cms_furniture_table.jpg" alt="">
+                </div>
+                <h4>餐桌·应用案例</h4>
+            </div>
+            <div class="list">
+                <?php $blog_cat_ID = get_cat_ID("桌子"); ?>
+                <?php query_posts("cat=$blog_cat_ID"); ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <a class="list-item" href="<?php echo get_the_excerpt(); ?>" target="_blank">
+                        <div class="cover-img">
+                            <img src="<?php post_thumbnail_src('thumbnail'); ?>" alt="">
+                        </div>
+                        <h5><?php the_title();?></h5>
+                        <p><?php the_content(); ?></p>
+                    </a>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+    <section class="chair furniture-item">
+        <div class="title">
+            <img src="https://oss.simright.com/images/cms_furniture_title-left.png" alt="">
+            <span>椅子</span>
+            <img src="https://oss.simright.com/images/cms_furniture_title-right.png" alt="">
+        </div>
+        <div class="content">
+            <div class="left">
+                <div class="cover-img">
+                    <img src="https://oss.simright.com/images/cms_funiture_chair.jpg" alt="">
+                </div>
+                <h4>椅子·应用案例</h4>
+            </div>
+            <div class="table-list">
+                <?php $blog_cat_ID = get_cat_ID("椅子"); ?>
+                <?php query_posts("cat=$blog_cat_ID"); ?>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <a class="list-item" href="<?php echo get_the_excerpt(); ?>" target="_blank">
+                        <div class="cover-img">
+                            <img src="<?php post_thumbnail_src('thumbnail'); ?>" alt="">
+                        </div>
+                        <h5><?php the_title();?></h5>
+                        <p><?php the_content(); ?></p>
+                    </a>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+</section>
+
+<?php get_footer(); ?>
