7ce0f88a0a85a87106952c922e3f1eb5479f3b4b
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-05-29 15:28:10 +0800

    fix(trans):添加threemagic的翻译

--- wp-content/themes/simright/functions.php
+++ wp-content/themes/simright/functions.php
@@ -181,6 +181,7 @@ if( function_exists('pll_register_string') ) {
 	pll_register_string('CAD Converter - CAD model converter','CAD Converter - CAD model converter');
 	pll_register_string('Viewer - CAD/CAE model viewer','Viewer - CAD/CAE model viewer');
 	pll_register_string('WebMesher – Pre-processor','WebMesher – Pre-processor');
+	pll_register_string('ThreeMagic - STL editor for 3D printing','ThreeMagic - STL editor for 3D printing');
 	pll_register_string('Resources','Resources');
 	pll_register_string('Public Projects','Public Projects');
 	pll_register_string('Model Library','Model Library');
