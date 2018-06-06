39c6ea4f8b00b572021c8ae0fcc41f6d08ed9c95
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-06-06 13:38:20 +0800

    fix(user story): 用户故事图片

--- wp-content/themes/simright/header-page.php
+++ wp-content/themes/simright/header-page.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v13.0" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v14.0" type="text/css" media="screen" />
 </head>
