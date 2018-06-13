cbce64dfc1b1709dd0eb0c59f189b6d28bc73d87
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-06-13 16:09:14 +0800

    feat(live):直播页面

---
+++ wp-content/themes/simright/category-live.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+<section class="main-box live-list">
+    <div class="banner">
+        <h2 class="text-center"> <?php pll_e('LIVE'); ?></h2>
+    </div>
+    <section class="about-live">
+        <div class="upcoming-live">
+            <h2 class="classify-title"><?php pll_e('ji jiang jin xing de zhi bo'); ?></h2>
+            <?php if(pll_current_language()=="zh"): ?>
+                <?php $blog_cat_ID = get_cat_ID("即将进行的直播"); ?>
+                <?php query_posts("cat=$blog_cat_ID"); ?>
+            <?php else: ?>
+                <?php $blog_cat_ID = get_cat_ID("upcoming-live"); ?>
+                <?php query_posts("cat=$blog_cat_ID"); ?>
+            <?php endif ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="live-item">
+                    <a href="<?php the_permalink(); ?>">
+                        <div class="img-cover" style="background-image:url('<?php echo post_thumbnail_src('thumbnail'); ?>');">
+                            <h2><?php echo the_title()?></h2>
+                        </div>
+                        <h3 class="time"><?php echo get_post_meta($post->ID, "time", $single = true) ?></h3>
+                        <div class="live-info">
+                            <?php echo the_excerpt();?>
+                        </div>
+                    </a>
+                </div>
+            <?php endwhile; ?>
+            <?php else : ?>
+                <h3 class="title"><a href="#" rel="bookmark">NOT FOUND</a></h3>
+            <?php endif; ?>
+        </div>
+        <div class="ended-live">
+            <h2 class="classify-title"><?php pll_e('yi jing jie shu de zhi bo'); ?></h2>
+            <?php if(pll_current_language()=="zh"): ?>
+                <?php $blog_cat_ID = get_cat_ID("已经结束的直播"); ?>
+                <?php query_posts("cat=$blog_cat_ID"); ?>
+            <?php else: ?>
+                <?php $blog_cat_ID = get_cat_ID("completed-live"); ?>
+                <?php query_posts("cat=$blog_cat_ID"); ?>
+            <?php endif ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="live-item live-item-ended">
+                    <a href="<?php the_permalink(); ?>">
+                        <div class="img-cover" style="background-image:url('<?php echo post_thumbnail_src('thumbnail'); ?>');">
+                            <h2><?php echo the_title()?></h2>
+                        </div>
+                        <h3 class="time"><?php echo get_post_meta($post->ID, "time", $single = true) ?></h3>
+                        <div class="live-info">
+                            <?php echo the_excerpt();?>
+                        </div>
+                        <a href="<?php echo get_post_meta($post->ID, "video-link", $single = true) ?>" class="btn btn-light" target="_blank"><?php pll_e('Watch Now')?></a>
+                    </a>
+                </div>
+            <?php endwhile; ?>
+            <?php else : ?>
+                <h3 class="title"><a href="#" rel="bookmark">NOT FOUND</a></h3>
+            <?php endif; ?>
+        </div>
+    </section>
+</section>
+<?php get_footer(); ?>
---
+++ wp-content/themes/simright/single-live.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+<section class="main-box live-details">
+    <div class="main-contain">
+        <a href="<?php echo get_option('home'); ?>/<?php echo pll_current_language() ?>/category/live"><?php pll_e('返回直播中心'); ?></a>
+        <hr/>
+        <?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+            <div class="details-contain">
+                <div class="contain-top">
+                    <div class="img-wrap" style="max-width:500px;max-height:300px;">
+                        <img src="<?php post_thumbnail_src('thumbnail'); ?>"  alt="" style="max-width:100%;max-height:100%">
+                    </div>
+                    <div class="live-info">
+                        <h3 class="title"><?php the_title(); ?></h3>
+                        <div class="tips">
+                            <span class="label-infos"><?php pll_e('直播时间'); ?>:</span>
+                            <span><?php echo get_post_meta($post->ID, "time", $single = true) ?></span>
+                        </div>
+                        <div class="tips">
+                            <span class="label-infos"><?php pll_e('适用人群'); ?>:</span>
+                            <span><?php echo get_post_meta($post->ID, "applicable-people", $single = true) ?></span>
+                        </div>
+                        <div class="tips number">
+                            <span class="label-infos"><?php pll_e('已报名'); ?>:</span>
+                            <span><b><?php echo get_post_meta($post->ID, "number", $single = true) ?></b><?php pll_e('人'); ?></span>
+                        </div>
+                        <a href="#" class="btn btn-light btn-sm"><?php pll_e('立即报名'); ?></a>
+                    </div>
+                </div>
+                <div class="contain-bottom">
+                    <div class="contain-text">
+                        <h2 class="title"><?php pll_e('直播内容'); ?></h2>
+                        <?php the_content(); ?>
+                    </div>
+                    <div class="liver-info">
+                        <div class="info-header">
+                            <img src="<?php echo get_post_meta($post->ID, "person-avatar", $single = true) ?>" alt="">
+                            <div>
+                                <h4><?php echo get_post_meta($post->ID, "person-name", $single = true) ?></h4>
+                                <p><?php echo get_post_meta($post->ID, "person-title", $single = true) ?></p>
+                            </div>
+                        </div>
+                        <div class="person-info">
+                            <?php echo get_post_meta($post->ID, "person-info", $single = true) ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php else : ?>
+            <p>error</p>
+        <?php endif; ?>
+    </div>
+</section>
+<?php get_footer(); ?>
