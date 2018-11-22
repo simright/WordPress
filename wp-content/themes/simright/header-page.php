c1dbf0c94b9bfa1d3e51375ad41586314c33851a
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-11-22 17:52:00 +0800

    fix:清样式缓存

--- wp-content/themes/simright/header-page.php
+++ wp-content/themes/simright/header-page.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v26.0" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v27.0" type="text/css" media="screen" />
 </head>
