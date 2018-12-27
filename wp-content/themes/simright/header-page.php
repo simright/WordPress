bb396f7b43217092146804db011a4365c2faf3ab
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-12-27 15:10:11 +0800

    fix:icon width

--- wp-content/themes/simright/header-page.php
+++ wp-content/themes/simright/header-page.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v28.0" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v29.0" type="text/css" media="screen" />
 </head>
