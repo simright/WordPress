195dd76c3f19c95b02308508a6acf9fbca0b1680
Qmengxi <mengxiaoxi_qian@sina.cn>
2019-05-16 09:59:00 +0800

    fix: copyright change to 2019

--- wp-content/themes/simright/footer-structural.php
+++ wp-content/themes/simright/footer-structural.php
@@ -77,7 +77,7 @@
     </section>
     <section class="clearfix copyright">
         <p class="text-center">
-            Copyright © 2018 Simright Information Technology Co.,Ltd. All Rights Reserved.
+            Copyright © 2019 Simright Information Technology Co.,Ltd. All Rights Reserved.
         <p class="text-center">Powered By <a rel="external" title="WordPress" class="link" href="http://wordpress.org/">WordPress</a></p>
         <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273840646'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1273840646%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
     </section>
